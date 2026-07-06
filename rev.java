210af5fc79819462ff96655025374134c0a11661
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:15:57 +0530

    reverse the number

---
+++ rev.java
@@ -0,0 +1,19 @@
+// public class rev {
+//   public static void main(String[] args) {
+// // String n="12345";
+// //     for(int i=n.length()-1; i>=0; i--){
+// //         System.out.print(n.charAt(i));
+// //     }
+// //   }  
+// // }
+// int n=12345;
+// int rev=0;
+// while (n!=0) {
+//       int lastDigit=n%10;
+//       rev *=10;
+//       rev +=lastDigit;
+//       n/=10;
+// }
+// System.out.println(rev);
+//   }
+// }
