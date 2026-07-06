902d5fd637b89af1068511d7abf93f694e1642db
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:15:29 +0530

    product of number usinf loops

---
+++ product.java
@@ -0,0 +1,23 @@
+
+// import java.util.*;
+// public class product {
+//     public static void main(String[] args) {
+//         System.out.println("entr n :");
+       
+//         Scanner sc= new Scanner(System.in) ;
+//          int n=sc.nextInt();
+
+//         //int sum=0;
+
+//            int product=1;
+
+//            while(n>0){
+//             product *=(n % 10);
+//             n /=10;
+
+//            }
+//            System.out.println(" product of digit = "+product);
+//      }
+     
+//     }
+
