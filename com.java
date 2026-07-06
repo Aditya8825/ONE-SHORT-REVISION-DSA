212380b0522a6fda9a855694d0d13fd153454a87
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:14:00 +0530

    compositive and even number check

---
+++ com.java
@@ -0,0 +1,26 @@
+
+// import java.util.*;
+// public class com {
+
+//     public static void main(String[] args) {
+//         Scanner sc= new  Scanner(System.in);
+//         System.out.println("enter n: ");
+//         int n=sc.nextInt();
+//            boolean flag=false;
+
+//         for(int i=2; i<n; i++){
+//             if(n%i==0){
+//                 flag=true;
+//                 //System.out.println(" composite number ");
+//                 break;
+//             }
+//         }
+//         if(flag==false){
+
+//     System.out.println(" prime number :");
+// }  
+// else{
+//     System.out.println(" composite number : 89");
+// }
+//   }
+// }
