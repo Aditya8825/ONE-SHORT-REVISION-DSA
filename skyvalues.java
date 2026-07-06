60bc96427dc41fc98979c4663be8b22ed4d10d55
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:16:24 +0530

    revsre the number usinf loops

---
+++ skyvalues.java
@@ -0,0 +1,9 @@
+
+
+// public class skyvalues {
+//     public static void main(String[] args) {
+//         for(int i=10; i<=67; i++){
+//             System.out.println(i+" "+(char)i);
+//         }
+//     }
+// }
