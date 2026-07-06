0b59cc545f165954e272e0db70ec98a293583e9a
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:14:25 +0530

    factorial check

---
+++ factorial.java
@@ -0,0 +1,13 @@
+
+// public class factorial {
+//   public static void main(String[] args) {
+//     int n=6;
+//     int fact=1;
+//     for(int i=1; i<=n; i++){
+//         fact =fact*i;
+//         System.out.println(i+"! = "+fact);
+
+//     }
+//     System.out.println();
+//   }  
+// }
