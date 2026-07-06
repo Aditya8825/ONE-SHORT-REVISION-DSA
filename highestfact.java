d220543982aab1b958d839e27a17be8c9f201fbc
Aditya Raj <aditya998887766@gmail.com>
2026-07-06 18:14:51 +0530

    highest factorial

---
+++ highestfact.java
@@ -0,0 +1,22 @@
+// import java.util.*;
+// public class highestfact {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("enter n: ");
+
+//         int n=sc.nextInt();
+
+//        // int hf=1;
+// //         for(int i=1; i<n; i++){
+// //             if(n%i==0){
+// //                 hf=i;
+// //             }
+// //         }
+// //         System.out.println(hf);
+// //     }
+// // }
+// for(int i=n/2; i<n; i++){
+//       for(i%2==0){
+
+//       }
+// }}}
