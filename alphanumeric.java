06980407fc3c214451bfc4abdae61244e11fa623
Aditya Raj <aditya998887766@gmail.com>
2026-07-07 18:12:16 +0530

    alpha numeric patern parblom

---
+++ alphanumeric.java
@@ -0,0 +1,20 @@
+// public class alphanumeric {
+
+//     public static void main(String[] args) {
+//        int n=8;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 if(i%2!=0) System.out.println(j+" ");
+//               else  System.out.print((char)(j+64)+" ");
+//               // System.out.print(j);
+//             }
+//              System.out.println();
+//             // for(int j=1; j<=i; j++){
+//             //      System.out.println(j);
+//             }
+//           //  System.out.println();
+//             // System.out.println();
+        
+//     }
+// }
+
