b019c53bde8a44c1c165898cb57ce19217968d7c
Aditya Raj <aditya998887766@gmail.com>
2026-07-07 22:34:03 +0530

    hollow rectangle pattern

---
+++ hollowrectangle.java
@@ -0,0 +1,16 @@
+// public class hollowrectangle {
+//     public static void main(String[] args) {
+//         int n=6;//j
+//         int m=4;//i
+//         for(int  i=1; i<=m; i++){
+//             for(int j=1; j<=n; j++){
+//                 if(i==1 || i==m ||j==1 || j==n){
+//                     System.out.print("* ");
+//                 }else{
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
