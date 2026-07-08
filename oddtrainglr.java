5a16d4cf6e350b3441dc2bcee5443bfabf54abed
Aditya Raj <aditya998887766@gmail.com>
2026-07-08 22:33:01 +0530

    odd traingle pattern

---
+++ oddtrainglr.java
@@ -0,0 +1,13 @@
+// public class oddtrainglr {
+//     public static void main(String[] args) {
+//         int n=6;
+//          for(int i=1; i<=n; i++){
+//             if(i%2!=0){
+//                  for(int j=1; j<=i; j++){
+//                     System.out.print(i+" ");
+//                  }
+//                   System.out.println();
+//             }
+//          }
+//         }
+//     }
