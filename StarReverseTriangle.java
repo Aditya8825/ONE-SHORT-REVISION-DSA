420e5348802badd34caf157cb0abb39ac268ad76
Aditya Raj <aditya998887766@gmail.com>
2026-07-09 23:10:13 +0530

    revsre trainge

---
+++ StarReverseTriangle.java
@@ -0,0 +1,39 @@
+// public class starrevrsetraingle {
+//    public static void main(String[] args) {
+    
+  
+//        int n=6;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print(" ");
+//             }
+//            for(int j=i; j<=n; j++){
+//             System.out.print("* ");
+//            }
+//            System.out.println();
+//         } 
+//        // System.out.println();
+//     }
+// }
+
+
+
+// public class StarReverseTriangle {
+//     public static void main(String[] args) {
+//         int n = 6;
+//         for (int i = 1; i <= n; i++) {
+//             // Print leading spaces
+//             for (int j = 1; j < i; j++) {
+//                 System.out.print("# ");
+//             }
+//             // Print stars
+//             for (int j = i; j <= n; j++) {
+//                 System.out.print("* ");
+//             }
+
+//             System.out.println();
+//         }
+//     }
+// }
+
+   
