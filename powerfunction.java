2494276408dbd6e5f0d0a04563da5644d93f8218
Aditya Raj <aditya998887766@gmail.com>
2026-07-09 23:11:53 +0530

    power function usinf mejthid

---
+++ powerfunction.java
@@ -0,0 +1,15 @@
+// import java.lang.invoke.MethodHandle;
+
+// public class powerfunction{
+//     public static void main(String [] args){
+//         // int a=2;
+//         // int b=5;
+
+//        // int power=1;
+//         // for(int i=1; i<=b; i++){
+//         //     power *=a;
+
+//         System.out.println(Math.pow(4,3));
+//         }
+//           //System.out.println(power);
+//     }
