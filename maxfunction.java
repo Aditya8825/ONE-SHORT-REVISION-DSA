5827dbccea8670e0ab4270b23da12030c02938df
Aditya Raj <aditya998887766@gmail.com>
2026-07-09 23:11:30 +0530

    max function methid

---
+++ maxfunction.java
@@ -0,0 +1,6 @@
+// public class maxfunction {
+//     public static void main(String[] args) {
+//         int a=5, b=8,c=2;
+//           System.out.println(Math.max(a,Math.max(b,c)));
+//     }
+// }
