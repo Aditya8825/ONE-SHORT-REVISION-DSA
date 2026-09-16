c12c6f2fee302eeafc3b998868a7b3d51f80807b
Aditya Raj <aditya998887766@gmail.com>
2026-09-16 15:04:22 +0530

    largest factor of a number

---
+++ largestfact.java
@@ -0,0 +1,10 @@
+// public class largestfact {
+//     public static void main(String[] args) {
+//         int n=60;
+//         for(int i=n/2; i>=1; i--){
+//             if(n%i==0) System.out.println("highest factor = "+i);
+//             break;
+            
+//         }
+//     }
+// }
