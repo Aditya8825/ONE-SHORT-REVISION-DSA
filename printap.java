9edfa4927aacadc7d0f0c9a002ebe85b4c35f5bf
Aditya Raj <aditya998887766@gmail.com>
2026-09-16 15:04:48 +0530

    write a code to ap

---
+++ printap.java
@@ -0,0 +1,11 @@
+// public class printap {
+//     public static void main(String[] args) {
+//         int a=3;
+//         int b=5;
+//         int d =b-a;
+//         int n=10;
+//         for(int i=0; i<n; i++){
+//             System.out.println(a+i*d);
+//         }
+//         }
+//     }
