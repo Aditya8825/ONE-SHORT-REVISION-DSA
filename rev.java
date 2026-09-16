b25005c15a601f6942ab5b35bac2c20cdc7dbc20
Aditya Raj <aditya998887766@gmail.com>
2026-09-16 15:05:49 +0530

    print reverse number

--- rev.java
+++ rev.java
@@ -1,19 +1,14 @@
-// public class rev {
-//   public static void main(String[] args) {
-// // String n="12345";
-// //     for(int i=n.length()-1; i>=0; i--){
-// //         System.out.print(n.charAt(i));
-// //     }
-// //   }  
-// // }
-// int n=12345;
-// int rev=0;
-// while (n!=0) {
-//       int lastDigit=n%10;
-//       rev *=10;
-//       rev +=lastDigit;
-//       n/=10;
-// }
-// System.out.println(rev);
-//   }
-// }
+//  public class rev{
+//     public static void main(String[] args) {
+//         long n=1534236469;
+//         long rev=0;
+//         while(n!=0){
+//             long digit=n%10; // GET LAST NUMBER         }
+//             rev=rev*10+digit;
+//             n=n/10; // remove last number 
+
+
+//     }
+//     System.out.println(" revse number = "+rev);
+// }}
+
