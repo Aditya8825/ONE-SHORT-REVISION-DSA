c2cec47aafd88e1dcbaa50e954d8b1155692f0af
Aditya Raj <aditya998887766@gmail.com>
2026-09-16 15:03:04 +0530

    check even or odd number

---
+++ even.java
@@ -0,0 +1,18 @@
+// public class even {
+//     public static void main(String[] args) {
+//         int a=1;
+//         int b=100;
+        
+//          for(int i=a; i<=b; i++){
+//             if(i%2==0){
+//                 System.out.println("even bumber "+i);
+//             }else{
+//                 System.out.println("odd number :"+i);
+//             }
+//         }
+//               //  System.out.println(i);
+//             // }else{
+//             //     System.out.println("odd number: ");
+//             }
+//          }
+    
