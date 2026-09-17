e9ddac285f346db05d0649a9eea4be2c7aceb42a
Aditya Raj <aditya998887766@gmail.com>
2026-09-17 23:04:34 +0530

    sqaure pattern

---
+++ sqaure.java
@@ -0,0 +1,11 @@
+// public class sqaure {
+//     public static void main(String[] args) {
+//         int n=8;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=n; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }//System.out.println();
+//     }
+// }
