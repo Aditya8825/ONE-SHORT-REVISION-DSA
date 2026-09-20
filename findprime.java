0cc9303109a30d15cccf48f9e03928e45ffddfb7
Aditya Raj <aditya998887766@gmail.com>
2026-09-20 21:24:10 +0530

    METHOD TO FIND THE PRIME NUMBER

---
+++ findprime.java
@@ -0,0 +1,32 @@
+// import java.util.*;
+// public class findprime {
+//     static boolean isprime(int n){
+//         if (n<=1){
+//             return  false;
+//         }
+//         for(int i=2; i<=n/2; i++){
+//             if(n%i==0){
+//                 return  false;
+//             }
+//         }
+//         return  true;
+//     }
+//     static void printprime(int  start,int end){
+//         for(int i=start; i<=end; i++){
+//             if(isprime(i)){
+//                 System.out.print(i+" ");
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("enter start:");
+//         int start=sc.nextInt();
+
+//         System.out.println("enter end :");
+//         int end=sc.nextInt();
+
+     
+//      printprime(start,end);
+//     }
+// }
