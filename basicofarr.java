48c3744fe69356867e42db4948ffbf2f9a0dbaf2
Aditya Raj <aditya998887766@gmail.com>
2026-09-22 22:40:03 +0530

    basic of Array

---
+++ basicofarr.java
@@ -0,0 +1,20 @@
+// import java.util.Scanner;
+
+// public class basicofarr {
+//    public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+
+//     System.out.print("enter arr length ");
+//     int length=sc.nextInt();
+//       int []arr= new int[length];
+
+//     System.out.print("enter arr element:");
+//     for(int i=0;  i<length; i++){
+//         arr[i]=sc.nextInt();
+//     }
+//        System.out.print("your array is : ");
+//        for(int i=0; i<length; i++){
+//         System.out.println(arr[i]);
+//        }
+//    } 
+// }
