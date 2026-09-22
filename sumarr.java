9b1bc0320321bf004311121b3eec0172ce46426e
Aditya Raj <aditya998887766@gmail.com>
2026-09-22 22:58:13 +0530

    sum of arr

---
+++ sumarr.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+// public class sumarr {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("entr array length: ");
+//         int n=sc.nextInt();
+//         int []arr=new int[n];
+
+//         System.out.print("enter arr element:");
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int sum=0;
+//         System.out.print("sum of arr is");
+//         for(int i=0; i<n; i++){
+//             sum+=arr[i];
+             
+//         }
+//     System.out.print(sum+" ");
+//     }
+// }
