2c703f451be87ebc6f6038b6a501d51d6eee3221
Aditya Raj <aditya998887766@gmail.com>
2026-09-22 22:57:49 +0530

    product of arr

---
+++ productofarr.java
@@ -0,0 +1,22 @@
+
+// import java.util.*;
+// public class productofarr {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("entr array length: ");
+//         int n=sc.nextInt();
+//         int []arr=new int[n];
+
+//         System.out.print("enter arr element:");
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int product=1;
+//         System.out.print("product  of arr is:");
+//         for(int i=0; i<n; i++){
+//             product=product*arr[i];
+             
+//         }
+//     System.out.print(product+" ");
+//     }
+// }
