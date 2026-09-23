67bd1f8cf23116419664ad05d128950a52217953
Aditya Raj <aditya998887766@gmail.com>
2026-09-23 22:41:39 +0530

    snoll in java

---
+++ sorted.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+// public class sorted {
+//     public static void main(String[] args) {
+//         int[]arr={4,1,7,5,-3,10,2};
+//         Arrays.sort(arr);
+//         print(arr); 
+//     }
+//     public static void print(int[]arr){
+//         for(int i=0; i<arr.length; i++){
+//         System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// }
