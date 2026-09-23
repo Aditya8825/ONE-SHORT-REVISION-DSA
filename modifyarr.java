2d3e9c9711fe0c39ae346ed2fb137b8b176c44a4
Aditya Raj <aditya998887766@gmail.com>
2026-09-23 22:41:10 +0530

    somequestion based on java

---
+++ modifyarr.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+// public class modifyarr {
+//     public static void main(String[] args) {
+//       Scanner sc= new Scanner(System.in);
+//       System.out.print("enter length of arr:");
+//       int n=sc.nextInt();
+//       int []  arr=new int[n];
+
+//       System.out.print("enter arr element: ");
+//       for(int i=0; i<n; i++){
+//         arr[i]=sc.nextInt();
+//       }
+//       for(int i=0; i<n; i++){
+//         if(i%2==0){
+//             arr[i]=arr[i]+10;
+                
+//       }else{
+//         arr[i]=arr[i]*2;
+//       }
+//     }
+//     System.out.println(Arrays.toString(arr));
+//     System.out.println("modify array:"+Arrays.toString(arr));
+// }
+// }
